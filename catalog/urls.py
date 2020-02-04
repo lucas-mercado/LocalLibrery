be2a983eaad6f55f1cc00a4ccb6c09852b6c674d
lucas-mercado <lucasemercado21@gmail.com>
2020-02-04 17:19:18 -0300

    cambios en view.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [url(r'^$', views.index, name='index'),]
 
 #lista libros
 urlpatterns += [url(r'^book/$', views.BookListView.as_view(), name='books'),]
-#urlpatterns += [url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),]
+urlpatterns += [url('<int:book_id>', views.BookDetailView.as_view(), name='book-detail'),]
 
 #lista autor
 urlpatterns += [url(r'^author/$', views.AuthorListView.as_view(), name='authors'), ]
--- catalog/views.py
+++ catalog/views.py
@@ -30,13 +30,13 @@ def index(request):
 class BookListView(generic.ListView):
     model = Book
     paginate_by = 10
-    context_object_name = 'book_list'   # your own name for the list as a template variable
-    queryset = Book.objects.all()
-    template_name = 'books/book_list.html'  # Specify your own template name/location
 
-    #def get_queryset(self):
-        # Get 5 books containing the title war
-    #    return Book.objects.filter(title__icontains='war')[:5]
+    #variable que se va usar en la template
+    context_object_name = 'book_list'
+    #consulta
+    queryset = Book.objects.all()
+    #template ubicacion/nombre
+    template_name = 'books/book_list.html'  # Specify your own template name/location
     
     #Cuando se hace esto es importante seguir este patrón:
     #Primero obtener el contexto existente desde nuestra superclase.
@@ -53,20 +53,17 @@ class BookListView(generic.ListView):
 class BookDetailView(generic.DetailView):
     model = Book
 
-
-def book_detail_view(request, pk):
-    try:
-        book_id = Book.objects.get(pk=pk)
-    except Book.DoesNotExist:
-        raise Http404("Book does not exist")
-
-    #book_id=get_object_or_404(Book, pk=pk)
-
-    return render(
-        request,
-        'catalog/book_detail.html',
-        context={'book': book_id, }
-    )
+    def book_detail_view(request, pk):
+        try:
+            book_id = Book.objects.get(pk=pk)
+        except Book.DoesNotExist:
+            raise Http404("Book does not exist")
+        #book_id=get_object_or_404(Book, pk=pk)
+        return render(
+            request,
+            'book-detail/book_detail.html',
+            context={'book': book_id, }
+        )
 
 
 class AuthorListView(generic.ListView):
